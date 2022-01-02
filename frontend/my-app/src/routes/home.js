4c1deb9d59338036e448fec62fb3da9678eada76
Tamara Wenner <tamara-76@hotmail.fr>
2022-01-03 00:24:20 +0100

    search icon added to header search bar

--- frontend/my-app/src/routes/home.js
+++ frontend/my-app/src/routes/home.js
@@ -1,26 +1,11 @@
 import '../sass/App.scss';
 import Header from '../components/header';
-import Nav from '../components/nav';
-import ContactContainer from '../components/contact-container';
-import maleAvatar from '../assets/male-avatar-profile.jpg';
 
 
 function Home() {
   return (
     <div className="App">
       <Header />
-      <main>
-        <article className='left-container'>
-          <Nav />
-        </article>
-        <div>
-          Home
-        </div>
-        <div className='right-container'>
-          <h2>CONTACT</h2>
-          <ContactContainer />
-        </div>
-      </main>
     </div>
   );
 }
