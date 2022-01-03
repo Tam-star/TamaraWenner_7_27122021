a15fbf903fb3f11460a5064a4a404bb663d9b2bf
Tamara Wenner <tamara-76@hotmail.fr>
2022-01-03 01:40:42 +0100

    nav and post form changed

--- frontend/my-app/src/components/nav.js
+++ frontend/my-app/src/components/nav.js
@@ -4,12 +4,12 @@ export default function Nav() {
     return (
         <nav className='menu'>
             <ul>
-                <li><i className="fas fa-home"></i> Home</li>
-                <li><i className="fas fa-user-friends"></i> People</li>
-                <li><i className="fas fa-images"></i> Photos</li>
-                <li><i className="far fa-newspaper"></i> News Feed</li>
-                <li><i className="fas fa-user"></i> Profile</li>
-                <li><i className="fas fa-cog"></i> Settings</li>
+                <li className='menu__element'><i className="fas fa-home"></i> Home</li>
+                <li className='menu__element'><i className="fas fa-user-friends"></i> People</li>
+                <li className='menu__element'><i className="fas fa-images"></i> Photos</li>
+                <li className='menu__element'><i className="far fa-newspaper"></i> News Feed</li>
+                <li className='menu__element'><i className="fas fa-user"></i> Profile</li>
+                <li className='menu__element menu__element--no-border'><i className="fas fa-cog"></i> Settings</li>
             </ul>
         </nav>
     )
