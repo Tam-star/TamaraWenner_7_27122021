7a9016cb0e7e164f83b4bc4c32fbc103fa903a7a
Tamara Wenner <tamara-76@hotmail.fr>
2022-01-08 15:59:38 +0100

    Modified the order of route for logout to work

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,14 +1,12 @@
 const jwt = require('jsonwebtoken');
 
-
-
 module.exports = (req, res, next) => {
     try {
         if(!req.cookies['groupomania-jwt']){
             const message = 'Vous n\'êtes pas autorisé à effectuer cette action'
             return res.status(401).json({ message })
         }
-        //On récupère le token dans le cookie directement plutôt que dans le header
+        //On récupère le token dans le cookie 
         const token = req.cookies['groupomania-jwt']
         const decodedToken = jwt.verify(token, 'GROUPOMANIA_SECRET_TOKEN');
         const userId = decodedToken.userId;
@@ -25,24 +23,3 @@ module.exports = (req, res, next) => {
     }
 };
 
-// module.exports = (req, res, next) => {
-//     try {
-//         if(!req.headers.authorization){
-//             const message = 'Un token est nécessaire pour cette action'
-//             return res.status(401).json({ message })
-//         }
-//         const token = req.headers.authorization.split(' ')[1];
-//         const decodedToken = jwt.verify(token, 'GROUPOMANIA_SECRET_TOKEN');
-//         const userId = decodedToken.userId;
-//         req.auth = { userId: userId };
-//         if (req.body.userId && req.body.userId !== userId) {
-//             console.log('Invalid user ID')
-//             throw new Error('Invalid user ID');
-
-//         } else {
-//             next();
-//         }
-//     } catch (error){
-//         res.status(401).json({ message : error.message })
-//     }
-// };
