a264ba2ae81c80112dbdade453626d638df4b765
Tamara Wenner <tamara-76@hotmail.fr>
2022-01-18 19:42:42 +0100

    Added rate-limiter for login

---
+++ backend/middlewares/rate-limit.js
@@ -0,0 +1,10 @@
+const rateLimit = require("express-rate-limit");
+
+const loginLimiter = rateLimit({
+    windowMs: 30 * 60 * 1000, // 30 minutes
+    max: 3, // limit each IP to 3 requests per windowMs
+    message:
+        "Le nombre de tentative est limitée à 3, veuillez ré-essayez dans 30 minutes"
+});
+
+module.exports = loginLimiter;
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -2,13 +2,14 @@ const express = require('express')
 const router = express.Router()
 const auth = require('../middlewares/auth');
 const multer = require('../middlewares/multer-users');
+const limiter = require('../middlewares/rate-limit');
 const userCtrl = require('../controllers/user.controller')
 
 router.get('/', auth, userCtrl.getAllUsers)
 router.get('/logout', userCtrl.logout)
 router.get('/:id', auth, userCtrl.getUserById)
 router.post('/signup', userCtrl.signUp)
-router.post('/login', userCtrl.login)
+router.post('/login', limiter, userCtrl.login)
 router.put('/:id', auth, multer, userCtrl.modifyUser)
 router.delete('/:id', auth, userCtrl.deleteUser)
 
