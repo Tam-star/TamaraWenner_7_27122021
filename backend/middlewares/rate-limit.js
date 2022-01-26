6f117d2d7afa616c9cc0bd2ae36040527094cd0f
Tamara Wenner <tamara-76@hotmail.fr>
2022-01-26 16:38:35 +0100

    Fixed issue ith rate limiter by skipping successful requests

--- backend/middlewares/rate-limit.js
+++ backend/middlewares/rate-limit.js
@@ -3,6 +3,7 @@ const rateLimit = require("express-rate-limit");
 const loginLimiter = rateLimit({
     windowMs: 30 * 60 * 1000, // 30 minutes
     max: 3, // limit each IP to 3 requests per windowMs
+    skipSuccessfulRequests : true,
     message:
         "Le nombre de tentative est limitée à 3, veuillez ré-essayez dans 30 minutes"
 });
